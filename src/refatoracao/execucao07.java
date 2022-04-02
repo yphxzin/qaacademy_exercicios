a8b46742e104e5b939f9ea7581f482cf8831208b
yphxzin <pedrohmarqued@gmal.com>
2022-04-02 14:37:04 -0300

    ex7

---
+++ src/refatoracao/execucao07.java
@@ -0,0 +1,9 @@
+package refatoracao;
+
+public class execucao07 {
+    public static void main(String[] args) {
+        exercicio07 exercicio07= new exercicio07();
+        System.out.println(exercicio07.calcularInss(1045.00));
+    }
+    
+}
---
+++ src/refatoracao/exercicio07.java
@@ -0,0 +1,33 @@
+package refatoracao;
+
+public class exercicio07 {
+    public double calcularInss(double salario) {
+        double inss;
+        if (salario <= 1045.00) {
+            inss = salario * 0.075;
+
+        }
+
+        if (salario >= 1045.01 && salario <= 2089.60) {
+            inss = salario * 0.09;
+
+        }
+
+        if (salario >= 2089.61 && salario <= 3134.40) {
+            inss = salario * 0.12;
+
+        }
+
+        if (salario >= 3134.41 && salario <= 6101.06) {
+            inss = salario * 0.14;
+
+        }
+
+        if (salario >= 6101.07) {
+            inss = 854.15;
+            return inss;
+        }
+        return salario;
+    }
+
+}
