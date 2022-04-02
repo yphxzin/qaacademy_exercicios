b91b7e46a2ac463be99bd9a221e9c06fb93634e9
yphxzin <pedrohmarqued@gmal.com>
2022-04-02 11:56:55 -0300

    ex4

---
+++ src/refatoracao/execucao04.java
@@ -0,0 +1,9 @@
+package refatoracao;
+
+public class execucao04 {
+    public static void main(String[] args) {
+        exercicio04 exercicio04= new exercicio04();
+        System.out.println(exercicio04.numeroDigitado(10));
+    }
+    
+}
---
+++ src/refatoracao/exercicio04.java
@@ -0,0 +1,7 @@
+ package refatoracao;
+ public class exercicio04 {
+     public String numeroDigitado(int numero){
+         return "o dobro do numero " + numero +" é:"+ numero * 2;
+     }
+    
+ }
