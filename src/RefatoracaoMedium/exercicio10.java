36e2964983ec02457d3d7f6d87daf56a477f93cc
yphxzin <pedrohmarqued@gmal.com>
2022-04-09 14:43:44 -0300

    refatoracao ex10

---
+++ src/RefatoracaoMedium/exercicio10.java
@@ -0,0 +1,34 @@
+package RefatoracaoMedium;
+
+public class exercicio10 {
+    public int calcularFibonacci(int termo) {
+        int i = 2;
+        int numDigitado = termo;
+        int[] vetFibonacci = new int[numDigitado];
+        vetFibonacci[0] = 0;
+        vetFibonacci[1] = 1;
+        while (i < numDigitado) {
+            vetFibonacci[i] = vetFibonacci[i - 1] + vetFibonacci[i - 2];
+            i++;
+        }
+        return vetFibonacci[termo-1];
+
+    }
+
+    public int[] calcularFibonacciVetor(int termo) {
+        int i = 2;
+        int numDigitado = termo;
+        int[] vetFibonacci = new int[numDigitado];
+        vetFibonacci[0] = 0;
+        vetFibonacci[1] = 1;
+        while (i < numDigitado) {
+            vetFibonacci[i] = vetFibonacci[i - 1] + vetFibonacci[i - 2];
+            i++;
+        }
+        return vetFibonacci;
+
+    }
+}
+
+    
+
