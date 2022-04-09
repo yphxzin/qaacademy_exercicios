304c6f0dfc9b0820b97cf525bc48e6f3283fd03b
yphxzin <pedrohmarqued@gmal.com>
2022-04-09 15:12:57 -0300

    refatoracao ex09

---
+++ src/RefatoracaoMedium/execucao09.java
@@ -0,0 +1,16 @@
+package RefatoracaoMedium;
+
+public class execucao09 {
+    public static void main(String[] args) {
+        
+        String [] vetorAlunos = {"Flavio", "Reinaldo", "Samuel", "Edmond", "Pedro"}; 
+        int [] vetorNumeroAlunos = {1,2,3,4,5}; 
+
+        exercicio09 exercicio09 = new exercicio09(); 
+        exercicio09.exibeAlunosENumeros(vetorAlunos,vetorNumeroAlunos);
+    }
+    
+}
+
+    
+
