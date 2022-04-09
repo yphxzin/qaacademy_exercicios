4ac4bc400b7539801534c2286074704bc532b181
yphxzin <pedrohmarqued@gmal.com>
2022-04-09 13:26:03 -0300

    refatoracao ex09

---
+++ src/RefatoracaoMedium/exercicio09.java
@@ -0,0 +1,13 @@
+package RefatoracaoMedium;
+
+public class exercicio09 {
+
+    public void exibeAlunosENumeros(String vetorAlunos[], int vetorNumeroAlunos[]) {
+        int i = 0;
+        while (i <= 4) {
+            System.out.println(vetorAlunos[i]);
+            System.out.println(vetorNumeroAlunos[i]);
+            i++;
+        }
+    }
+}
