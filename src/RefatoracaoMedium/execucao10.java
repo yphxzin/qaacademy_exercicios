e5a537e3cb80fba6cc3c79f263e7821c814980e0
yphxzin <pedrohmarqued@gmal.com>
2022-04-09 15:11:56 -0300

    refatoracao ex09

---
+++ src/RefatoracaoMedium/execucao10.java
@@ -0,0 +1,15 @@
+package RefatoracaoMedium;
+
+public class execucao10 {
+    public static void main(String[] args) {
+        exercicio10 exercicio10 = new exercicio10();
+        System.out.println("Exibe o valor do termo: " + exercicio10.calcularFibonacci(10));
+        exibirVetor(exercicio10.calcularFibonacciVetor(10));
+    }
+
+    private static void exibirVetor(int[] calcularFibonacciVetor) {
+    }
+        
+    }
+
+
